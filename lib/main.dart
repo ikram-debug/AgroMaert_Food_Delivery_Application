56f20df8d0988137816ec115d993eb659f7e55ba
Ikram UD DIN <ikramkhan383@gmail.com>
2024-11-30 10:24:16 -0800

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:my_app/screens/Splash_Screen/Splash_Screen.dart';
 import 'package:my_app/screens/auth/rejistration_provider.dart';
 import 'package:provider/provider.dart';
-import 'screens/auth/login_screen.dart';
+
 
 
 void main() async{
@@ -35,50 +36,7 @@ class ikram extends StatelessWidget {
     );
   }
 }
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
 
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    Future.delayed(
-      const Duration(seconds: 2),
-          () {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) =>  LoginScr()));
-      },
-    );
-    super.initState();
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.green,
-      body:Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assest/Image.png'),
-            SizedBox(height: 10),
-            Text(
-              'AgroMart',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight:FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-      ) ,
-    );
-  }
-}
 
 
 
